443938d409945b8e4f9f0863fc6f13da00be7f3c
Leandro Pineda <leandro@inorbit.ai>
2021-06-07 13:55:40 -0300

    Init package

---
+++ ros2mass/ros2mass.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros2mass.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ros2mass'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='leandro',
+    maintainer_email='leandro@inorbit.ai',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ros2mass = ros2mass.ros2mass:main'
+        ],
+    },
+)
