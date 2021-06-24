a278305f0d3a0544819478e598cad4cc6c10a682
Leandro Pineda <leandro@inorbit.ai>
2021-06-24 16:35:30 -0300

    Fixed setup.py version, description and license

--- mrai_sender_py/setup.py
+++ mrai_sender_py/setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     package_data={'': ['schema.json']},
     include_package_data=True,
-    version='0.1.0',
+    version='0.0.2',
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -17,8 +17,8 @@ setup(
     zip_safe=True,
     maintainer='InOrbit',
     maintainer_email='support@inorbit.ai',
-    description='ROS2 node implementing MassRobotics AMR Interoperability Standard',
-    license='TODO: License declaration',
+    description='ROS2 node implementing a MassRobotics AMR Interoperability Sender',
+    license='3-Clause BSD License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
