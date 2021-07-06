8157d4eb52687b3b5229d7e8921a20ce7782238b
Leandro Pineda <leandropineda.lp@gmail.com>
2021-07-06 00:13:17 -0300

    Increase spin timeout

--- massrobotics_amr_sender_py/test/test_mass_interop.py
+++ massrobotics_amr_sender_py/test/test_mass_interop.py
@@ -414,7 +414,7 @@ def test_massrobotics_amr_node_status_report_not_sent_on_invalid_schema(event_lo
 
     node.mass_status_report.data['operationalState'] = 'foobar'
 
-    rclpy.spin_once(node, timeout_sec=.1)
+    rclpy.spin_once(node, timeout_sec=1)
 
     # Try to send a status report with an invalid schema i.e. ``foobar`` operational
     # state is not an allowed value.
