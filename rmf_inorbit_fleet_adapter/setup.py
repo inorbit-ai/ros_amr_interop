2bbcb12372da0928ea98f3cf196c775fead6571b
Tomás Badenes <tomasbadenes@gmail.com>
2023-02-28 12:45:13 -0300

    Update version number in setup.py

--- rmf_inorbit_fleet_adapter/setup.py
+++ rmf_inorbit_fleet_adapter/setup.py
@@ -6,7 +6,7 @@ package_name = 'rmf_inorbit_fleet_adapter'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
