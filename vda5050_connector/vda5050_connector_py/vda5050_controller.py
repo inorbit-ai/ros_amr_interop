570e8e82cc12b281a8b06d65c77aa0f4024db787
Alyssa Agnissan <alyssa.agnissan@quantillion.io>
2025-04-18 12:06:55 +0200

    vda5050_connector: update operating mode from adapter's order state

--- vda5050_connector/vda5050_connector_py/vda5050_controller.py
+++ vda5050_connector/vda5050_connector_py/vda5050_controller.py
@@ -675,7 +675,8 @@ class VDA5050Controller(Node):
                 "distance_since_last_node": order_state.state.distance_since_last_node,
                 "battery_state": order_state.state.battery_state,
                 "errors": current_errors + order_state.state.errors,
-                "informations": order_state.state.informations
+                "informations": order_state.state.informations,
+                "operating_mode": order_state.state.operating_mode
             }
         )
 
